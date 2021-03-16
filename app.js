01ab7f97deb03ba8553fb6946842aa0a6d7b4fa1
danieljones32 <42987701+danieljones32@users.noreply.github.com>
2021-03-16 16:14:19 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for requests
+app.listen(5000, () => {
+  console.log("listening on port 5000");
+});
+
+// register view engine
+//we need to define a view engine,  we will use EJS
+//using app.set()
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
